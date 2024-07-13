df5acb8420f7c7695b85eabe2a5700e2f1db88b6
Julian L <julian.lawrance@gmail.com>
2024-07-14 00:14:19 +0100

    Use button instead of label for representing items in bag

--- scripts/bag_menu.gd
+++ scripts/bag_menu.gd
@@ -19,7 +19,10 @@ func _on_bag_added_item(new_item: Item, items: Array[Item]):
 	var count := len(items)
 	empty_label.visible = count <= 0
 
-	var new_label := Label.new()
-	new_label.text = new_item.name
+	var new_button := Button.new()
+	new_button.text = new_item.name
+	new_button.alignment = HORIZONTAL_ALIGNMENT_LEFT
 
-	item_list.add_child(new_label)
+	item_list.add_child(new_button)
+
+	new_button.call_deferred("grab_focus")
