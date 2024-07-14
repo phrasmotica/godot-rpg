839910262ad2c0f4e4b72f5aba10d24eae6b9ad3
Julian L <julian.lawrance@gmail.com>
2024-07-14 18:04:05 +0100

    Remember focussed item when bag opens/closes

--- scripts/bag_menu.gd
+++ scripts/bag_menu.gd
@@ -9,6 +9,9 @@ var item_list: VBoxContainer
 @export
 var empty_label: Label
 
+var item_buttons: Array[Control]
+var current_item: Control
+
 func _ready():
 	if bag:
 		bag.added_item.connect(_on_bag_added_item)
@@ -23,6 +26,19 @@ func _on_bag_added_item(new_item: Item, items: Array[Item]):
 	new_button.text = new_item.name
 	new_button.alignment = HORIZONTAL_ALIGNMENT_LEFT
 
+	new_button.focus_entered.connect(
+		func():
+			current_item = new_button
+	)
+
 	item_list.add_child(new_button)
+	item_buttons.append(new_button)
 
 	new_button.call_deferred("grab_focus")
+
+func _on_visibility_changed():
+	if not current_item and len(item_buttons) > 0:
+		current_item = item_buttons[0]
+
+	if current_item:
+		current_item.grab_focus()
