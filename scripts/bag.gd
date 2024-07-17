fa4a836009c7d12bb0f5a212473c792ca2e0da1d
Julian Lawrance <julian.lawrance@gmail.com>
2024-07-17 08:00:57 +0100

    Use R key to add random item to bag

--- scripts/bag.gd
+++ scripts/bag.gd
@@ -8,12 +8,14 @@ var items: Array[Item] = []
 signal added_item(new_item: Item, items: Array[Item])
 
 func _process(_delta):
-	if Input.is_action_just_pressed("pick_up"):
-		add_item()
+	if Input.is_action_just_pressed("random_item"):
+		add_random_item()
 
-func add_item():
+func add_random_item():
 	var new_item := item_pool.get_random()
+	add_item(new_item)
 
+func add_item(new_item: Item):
 	items.append(new_item)
 
 	added_item.emit(new_item, items)
