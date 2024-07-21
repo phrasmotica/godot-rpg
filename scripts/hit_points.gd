7bb8ae74f53cf071db3bf594b58f28a7ae8c3796
Julian L <julian.lawrance@gmail.com>
2024-07-21 11:03:32 +0100

    Create scene for tracking hit points. Attach to player and make player stats panel update when the hit points change

---
+++ scripts/hit_points.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node
+
+@export_range(10, 100)
+var max_hp := 50:
+    set(value):
+        max_hp = value
+
+        if current_hp > max_hp:
+            # signal will be emitted by the current_hp setter
+            current_hp = max_hp
+        else:
+            current_hp_changed.emit(current_hp, max_hp)
+
+@export_range(10, 100)
+var current_hp := 50:
+    set(value):
+        current_hp = min(max_hp, value)
+
+        current_hp_changed.emit(current_hp, max_hp)
+
+signal current_hp_changed(hp: int, max_hp: int)
+
+func _ready():
+    current_hp = max_hp
---
+++ scripts/player_stats.gd
@@ -0,0 +1,9 @@
+@tool
+extends PanelContainer
+
+@export
+var hp_label: Label
+
+func _on_hit_points_current_hp_changed(hp: int, max_hp: int):
+	if hp_label:
+		hp_label.text = str(hp) + "/" + str(max_hp) + " HP"
