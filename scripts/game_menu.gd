f6a332f3b8a17079eaf98d9e6ecd923e97217975
Julian Lawrance <julian.lawrance@gmail.com>
2024-07-22 12:47:15 +0100

    Use generalised menu set logic in game menu script

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -1,33 +1,5 @@
 @tool
 extends MenuSet
 
-# HIGH: remove this hard dependency on the bag menu. Use generalised logic in
-# MenuSet class
-@export
-var bag_menu: Menu
-
-@export
-var bag_menu_dimmer: Dimmer
-
-func after_ready():
-	super.after_ready()
-
-	bag_menu.menu_disabled.connect(handle_bag_menu_disabled)
-	bag_menu.menu_enabled.connect(handle_bag_menu_enabled)
-
 func _on_visibility_changed():
 	set_process(visible)
-
-func handle_bag_menu_disabled(_menu: Menu):
-	print("BagMenu disabled, disabling GameMenu")
-
-	set_process(false)
-
-	bag_menu_dimmer.is_dimmed = true
-
-func handle_bag_menu_enabled(_menu: Menu):
-	print("BagMenu enabled, enabling GameMenu")
-
-	set_process(true)
-
-	bag_menu_dimmer.is_dimmed = false
--- scripts/menu_set.gd
+++ scripts/menu_set.gd
@@ -4,7 +4,7 @@ class_name MenuSet extends Menu
 @export
 var current_menu_index := -1:
 	set(value):
-		var new_index: int = max(-1, min(menu_dimmers.size() - 1, value))
+		var new_index: int = max(-1, min(menus.size() - 1, value))
 		var index_changed := current_menu_index != new_index
 
 		current_menu_index = new_index
@@ -14,15 +14,51 @@ var current_menu_index := -1:
 				menu_dimmers[i].is_dimmed = i != current_menu_index
 
 @export
-var menu_dimmers: Array[Dimmer] = []
+var menus: Array[Menu] = []:
+	set(value):
+		menus = value
+		update_configuration_warnings()
+
+@export
+var menu_dimmers: Array[Dimmer] = []:
+	set(value):
+		menu_dimmers = value
+		update_configuration_warnings()
 
 func after_ready():
-	if menu_dimmers.size() > 0:
+	if menus.size() > 0:
 		current_menu_index = 0
+
+	for i in range(menus.size()):
+		var menu = menus[i]
+
+		menu.menu_disabled.connect(
+			func(m):
+				print(m.name + " disabled, disabling " + name)
+
+				set_process(false)
+
+				menu_dimmers[i].is_dimmed = true
+		)
+
+		menu.menu_enabled.connect(
+			func(m):
+				print(m.name + " enabled, enabling " + name)
+
+				set_process(true)
+
+				menu_dimmers[i].is_dimmed = false
+		)
 
 func listen_for_inputs():
 	if Input.is_action_just_pressed("menu_set_cycle_next"):
-		current_menu_index = ((current_menu_index + 1) % menu_dimmers.size())
+		current_menu_index = ((current_menu_index + 1) % menus.size())
 
 	if Input.is_action_just_pressed("menu_set_cycle_previous"):
-		current_menu_index = ((current_menu_index + menu_dimmers.size() - 1) % menu_dimmers.size())
+		current_menu_index = ((current_menu_index + menus.size() - 1) % menus.size())
+
+func _get_configuration_warnings():
+	if menus.size() != menu_dimmers.size():
+		return ["Menu count and dimmer count must be the same!"]
+
+	return []
