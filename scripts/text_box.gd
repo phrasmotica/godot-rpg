035a03190ed5d6449267f990fc5de7723462a674
Julian Lawrance <julian.lawrance@gmail.com>
2024-07-30 13:07:46 +0100

    Start implementing crawling text box scene

---
+++ scripts/text_box.gd
@@ -0,0 +1,26 @@
+extends PanelContainer
+
+@export
+var fixed_speed := false
+
+@onready
+var label: Label = %Label
+
+func _ready():
+	label.visible_ratio = 0
+
+func _process(delta):
+	if fixed_speed:
+		progress_chars()
+	else:
+		progress_ratio(delta)
+
+func progress_chars():
+	if label.visible_ratio < 1:
+		# crawls 1 character per update
+		label.visible_characters += 1
+
+func progress_ratio(delta: float):
+	if label.visible_ratio < 1:
+		# all text will take 3 seconds to crawl
+		label.visible_ratio += delta / 3
