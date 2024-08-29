4f84163497b2479590f022b0264c780305711181
Julian L <julian.lawrance@gmail.com>
2024-08-29 15:11:23 +0100

    Upgrade project to Godot 4.3
    Squashed commit of the following:
    
    commit c6af2e38d0c1a3b8839f08d0c76d22e3a9180b23
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Thu Aug 29 13:30:56 2024 +0100
    
        Fix issues in tile detection due to deprecation of TileMap node. Also fix warnings and errors in other scripts
    
    commit 4ac47ce9356f5d4730ae9dad4b2c9c028a58b336
    Author: Julian L <julian.lawrance@gmail.com>
    Date:   Thu Aug 29 13:24:49 2024 +0100
    
        Upgrade project to Godot 4.3, including regeneration of tilemap

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export
-var tile_map: TileMap
+var tile_map_layer: TileMapLayer
 
 @export
 var tile_detection: TileDetection
@@ -12,7 +12,7 @@ signal player_faced_tile(tile: Tile)
 signal player_interacted(tile: Tile)
 
 func _on_player_position_faced(pos: Vector2):
-	player_facing_tile_data = tile_detection.get_tile_data(pos, tile_map)
+	player_facing_tile_data = tile_detection.get_tile_data(pos, tile_map_layer)
 
 	print("Player faced tile ID=" + str(player_facing_tile_data.id))
 
--- scripts/tile_detection.gd
+++ scripts/tile_detection.gd
@@ -1,9 +1,9 @@
 class_name TileDetection extends Node
 
-func get_tile_data(global_pos: Vector2, tile_map: TileMap) -> Tile:
-	var local_raycast_target := tile_map.to_local(global_pos)
+func get_tile_data(global_pos: Vector2, tile_map_layer: TileMapLayer) -> Tile:
+	var local_raycast_target := tile_map_layer.to_local(global_pos)
 
-	var tile := tile_map.local_to_map(local_raycast_target)
-	var tile_data := tile_map.get_cell_tile_data(0, tile)
+	var tile := tile_map_layer.local_to_map(local_raycast_target)
+	var tile_data := tile_map_layer.get_cell_tile_data(tile)
 
 	return tile_data.get_custom_data("tile") as Tile
