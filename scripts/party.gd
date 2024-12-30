187a9b887c6b3931b8633f06f73260ee571c2ae8
Julian L <julian.lawrance@gmail.com>
2024-12-30 00:04:13 +0000

    Add TODO item

--- scripts/party.gd
+++ scripts/party.gd
@@ -5,6 +5,9 @@ var members: Array[NPC] = []
 
 func follow_player(positions: Array[Vector2i]):
     for i in range(min(members.size(), positions.size())):
+        # TODO: make member n follow member n-1. This is the more general form
+        # of making each member move to the (i-n)th position of the player's
+        # movement history...
         var member := members[i]
         var pos := positions[i]
 
