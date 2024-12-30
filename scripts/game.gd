37302ac5f90f938860740991279290ac055b80d5
Julian L <julian.lawrance@gmail.com>
2024-12-30 11:08:57 +0000

    Create separate mapping context for player interaction

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export
+var ctx_interact: GUIDEMappingContext
+
+@export
 var walk_mode: GUIDEMappingContext
 
 func _ready() -> void:
+	GUIDE.enable_mapping_context(ctx_interact)
 	GUIDE.enable_mapping_context(walk_mode)
