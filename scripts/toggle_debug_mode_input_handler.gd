d5386f1ea78ec690d751bb2dba6bf5875d86e0a5
Julian L <julian.lawrance@gmail.com>
2025-01-02 18:39:46 +0000

    Hide GUIDE debug layer when the game starts

--- scripts/toggle_debug_mode_input_handler.gd
+++ scripts/toggle_debug_mode_input_handler.gd
@@ -9,5 +9,7 @@ var debug_layer: CanvasLayer
 func _ready() -> void:
     toggle.triggered.connect(_handle_toggle_triggered)
 
+    debug_layer.visible = false
+
 func _handle_toggle_triggered() -> void:
     debug_layer.visible = not debug_layer.visible
