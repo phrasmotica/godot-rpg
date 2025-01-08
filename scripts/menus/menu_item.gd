003e7f23a1f8eef5a5ea8890fa0be38eefc320ef
Julian Lawrance <julian.lawrance@gmail.com>
2025-01-08 22:41:35 +0000

    Simplify MenuItem script and scene

--- scripts/menus/menu_item.gd
+++ scripts/menus/menu_item.gd
@@ -2,49 +2,62 @@
 class_name MenuItem extends VBoxContainer
 
 @export
-var pointer: TextureRect
-
-@export
-var name_label: Label
-
-@export
 var text: String:
 	set(value):
 		text = value
 
-		if name_label:
-			name_label.text = text
+		_refresh()
 
 @export
 var selected := false:
 	set(value):
 		selected = value
 
-		if pointer:
-			pointer.visible = selected
+		_refresh()
 
 @export
 var disabled := false:
 	set(value):
+		var changed := disabled != value
 		disabled = value
 
-		name_label.modulate = Color.DARK_GRAY if disabled else Color.WHITE
+		_refresh()
 
-		if disabled:
-			on_disabled.emit()
-		else:
-			on_enabled.emit()
+		if changed:
+			if disabled:
+				on_disabled.emit()
+			else:
+				on_enabled.emit()
 
 @export
 var is_cancel := false:
 	set(value):
 		is_cancel = value
 
-		if name_label:
-			if is_cancel:
-				name_label.theme_type_variation = "CancelLabel"
-			else:
-				name_label.theme_type_variation = ""
+		_refresh()
+
+@onready
+var pointer: TextureRect = %SelectionPointer
+
+@onready
+var name_label: Label = %Name
 
 signal on_enabled
 signal on_disabled
+
+func _ready() -> void:
+	_refresh()
+
+func _refresh() -> void:
+	if name_label:
+		name_label.text = text
+
+		name_label.modulate = Color.DARK_GRAY if disabled else Color.WHITE
+
+		if is_cancel:
+			name_label.theme_type_variation = "CancelLabel"
+		else:
+			name_label.theme_type_variation = ""
+
+	if pointer:
+		pointer.visible = selected
