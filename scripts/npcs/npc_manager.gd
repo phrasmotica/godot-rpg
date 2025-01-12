099a7f9ddd46d31dc39bff8a5cb27e7db305113c
Julian L <julian.lawrance@gmail.com>
2025-01-12 10:26:24 +0000

    Stop tracking NPC when dialogue timeline finishes

--- scripts/npcs/npc_manager.gd
+++ scripts/npcs/npc_manager.gd
@@ -3,9 +3,15 @@ extends Node2D
 @export
 var player: Player
 
+@export
+var dialogue_manager: DialogueManager
+
 @onready
 var trade_tracker: NPCTradeTracker = %TradeTracker
 
 func _ready() -> void:
 	if player:
 		player.dialogue_triggered.connect(trade_tracker.track)
+
+	if dialogue_manager:
+		dialogue_manager.timeline_ended.connect(trade_tracker.handle_dialogue_ended)
--- scripts/npcs/npc_trade_tracker.gd
+++ scripts/npcs/npc_trade_tracker.gd
@@ -6,3 +6,16 @@ func track(npc: NPC) -> void:
     print("Tracking NPC: %s" % npc.name)
 
     _tracking_npc = npc
+
+func handle_dialogue_ended() -> void:
+    if not _tracking_npc:
+        return
+
+    var timeline: String = Dialogic.VAR.resume_with_timeline
+
+    if timeline.length() > 0:
+        print("Dialogue will resume with timeline %s, continuing to track NPC: %s" % [timeline, _tracking_npc.name])
+    else:
+        print("Dialogue ended, untracking NPC: %s" % _tracking_npc.name)
+
+        _tracking_npc = null
