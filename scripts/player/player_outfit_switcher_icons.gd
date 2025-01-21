205c4c2bb36f8ce436476642161aaf6427d490d6
Julian L <julian.lawrance@gmail.com>
2025-01-21 21:52:40 +0000

    Create scene containing icons that highlight different body parts of the player

---
+++ scripts/player/player_outfit_switcher_icons.gd
@@ -0,0 +1,20 @@
+@tool
+extends HBoxContainer
+
+@export
+var current_index: int:
+	set(value):
+		current_index = clampi(value, 0, icons.size() - 1)
+
+		_refresh()
+
+@export
+var icons: Array[TextureRect] = []:
+	set(value):
+		icons = value
+
+		_refresh()
+
+func _refresh() -> void:
+	for i in icons.size():
+		icons[i].modulate = Color.WHITE if i == current_index else Color.DIM_GRAY
