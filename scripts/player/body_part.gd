c88b4d7e88d51f80690aae76766a6d69ebf90c63
Julian Lawrance <julian.lawrance@gmail.com>
2025-01-23 22:43:33 +0000

    Create resource class for a body part, which then contains colour options. Create resources for player hair, torso and sleeves

---
+++ scripts/player/body_part.gd
@@ -0,0 +1,39 @@
+@tool
+class_name BodyPart extends Resource
+
+@export
+var part_name := "":
+    set(value):
+        part_name = value
+
+        emit_changed()
+
+@export
+var icon: Texture2D:
+    set(value):
+        icon = value
+
+        emit_changed()
+
+@export
+var colours: ColourOption:
+    set(value):
+        colours = value
+
+        emit_changed()
+
+func _ready() -> void:
+    if colours:
+        colours.changed.connect(emit_changed)
+
+func get_param_name() -> String:
+    return colours.param_name
+
+func get_colour() -> Color:
+    return colours.get_colour()
+
+func next_colour() -> void:
+    colours.next()
+
+func previous_colour() -> void:
+    colours.previous()
