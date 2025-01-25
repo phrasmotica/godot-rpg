fc49dc56a329bad4be6ecfbe33ae63e184e2f7d8
Julian L <julian.lawrance@gmail.com>
2025-01-25 18:08:52 +0000

    Create shader for UI transition with horizontal bars that grow upwards

--- scripts/ui/ui_transition.gd
+++ scripts/ui/ui_transition.gd
@@ -3,7 +3,7 @@ class_name UITransition extends ColorRect
 
 var _material := material as ShaderMaterial
 
-enum Type { FADE_IN, FADE_OUT }
+enum Type { FADE_IN, FADE_OUT, BARS_UP }
 
 @export
 var type: Type:
@@ -32,6 +32,9 @@ var fade_in_shader: Shader = load("res://resources/shaders/transition_fade_in.gd
 @onready
 var fade_out_shader: Shader = load("res://resources/shaders/transition_fade_out.gdshader")
 
+@onready
+var bars_up_shader: Shader = load("res://resources/shaders/transition_bars_up.gdshader")
+
 func _ready() -> void:
 	_refresh()
 
@@ -48,5 +51,8 @@ func _get_shader() -> Shader:
 
 	if type == Type.FADE_OUT:
 		return fade_out_shader
+
+	if type == Type.BARS_UP:
+		return bars_up_shader
 
 	return null
