6be416db3fc7d323ca6e5bf3d670094482af7a0b
Julian Lawrance <julian.lawrance@gmail.com>
2025-01-29 23:23:08 +0000

    Create "gate" scene for handling concurrent tasks that might take different amounts of time to complete. Use it to gate sets of UI transitions

--- scripts/ui/ui_transition_manager.gd
+++ scripts/ui/ui_transition_manager.gd
@@ -10,9 +10,12 @@ var map: Map
 @onready
 var ui_transition_scene: PackedScene = load("res://scenes/ui/ui_transition.tscn")
 
-# HIGH: create a scene for a generic transitions gate, and reuse it
-var _interaction_transitions_gate := 0
-var _item_transitions_gate := 0
+# HIGH: add more specific interaction/item functionality to the gate scenes
+@onready
+var _interaction_transitions_gate: UITransitionsGate = %InteractionTransitionsGate
+
+@onready
+var _item_transitions_gate: UITransitionsGate = %ItemTransitionsGate
 
 signal interaction_transitions_finished(tile: Tile)
 signal item_transitions_finished(item: Item)
@@ -28,55 +31,46 @@ func _ready() -> void:
         map.player_interacted.connect(_handle_map_player_interacted)
 
 func _handle_bag_before_consume_item(item: Item) -> void:
-    _item_transitions_gate = 0
+    _item_transitions_gate.reset()
 
     if item.consume_transitions.size() <= 0:
         item_transitions_finished.emit(item)
         return
 
     for t in item.consume_transitions:
-        _item_transitions_gate += 1
+        _item_transitions_gate.hold()
 
-        var transition: UITransition = ui_transition_scene.instantiate()
-        transition.inject(t)
+        var transition := _create_transition(t)
 
         transition.finished.connect(
             func():
-                _handle_item_transition_finished(item)
+                if _item_transitions_gate.release():
+                    item_transitions_finished.emit(item)
         , CONNECT_ONE_SHOT)
 
-        add_child(transition)
-        transition.owner = self
-
-func _handle_item_transition_finished(item: Item) -> void:
-    _item_transitions_gate -= 1
-
-    if _item_transitions_gate <= 0:
-        item_transitions_finished.emit(item)
-
 func _handle_map_player_interacted(tile: Tile) -> void:
-    _interaction_transitions_gate = 0
+    _interaction_transitions_gate.reset()
 
     if tile.transitions_on_interact.size() <= 0:
         interaction_transitions_finished.emit(tile)
         return
 
     for t in tile.transitions_on_interact:
-        _interaction_transitions_gate += 1
+        _interaction_transitions_gate.hold()
 
-        var transition: UITransition = ui_transition_scene.instantiate()
-        transition.inject(t)
+        var transition := _create_transition(t)
 
         transition.finished.connect(
             func():
-                _handle_interaction_transition_finished(tile)
+                if _interaction_transitions_gate.release():
+                    interaction_transitions_finished.emit(tile)
         , CONNECT_ONE_SHOT)
 
-        add_child(transition)
-        transition.owner = self
+func _create_transition(params: UITransitionParams) -> UITransition:
+    var transition: UITransition = ui_transition_scene.instantiate()
+    transition.inject(params)
 
-func _handle_interaction_transition_finished(tile: Tile) -> void:
-    _interaction_transitions_gate -= 1
+    add_child(transition)
+    transition.owner = self
 
-    if _interaction_transitions_gate <= 0:
-        interaction_transitions_finished.emit(tile)
+    return transition
