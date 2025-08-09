1dd397de023750d7bc8e05e5e2393ace73d5b049
Julian L <julian.lawrance@gmail.com>
2025-08-09 22:24:42 +0100

    Don't allow parent menus to uncover if dialogue is in progress

--- scripts/menus/child_menu_handler.gd
+++ scripts/menus/child_menu_handler.gd
@@ -12,7 +12,12 @@ var parent_menu: Menu
 @export
 var child_menus: Array[Menu] = []
 
+var _dialogue_in_progress := false
+
 func _ready() -> void:
+    dialogue_manager.timeline_started.connect(_set_dialogue_in_progress.bind(true))
+    dialogue_manager.timeline_ended.connect(_set_dialogue_in_progress.bind(false))
+
     parent_menu.steal_control.connect(_handle_parent_menu_steal_control)
 
     for m in child_menus:
@@ -23,6 +28,9 @@ func _ready() -> void:
             dialogue_manager.timeline_started.connect(m.cover)
             dialogue_manager.timeline_ended.connect(m.uncover)
 
+func _set_dialogue_in_progress(in_progress: bool) -> void:
+    _dialogue_in_progress = in_progress
+
 func _handle_parent_menu_steal_control(menu: Menu) -> void:
     print(menu.name + " stole control from " + str(child_menus.size()) + " child menu(s)")
 
@@ -32,7 +40,7 @@ func _handle_parent_menu_steal_control(menu: Menu) -> void:
 func _handle_child_menu_hidden(menu: Menu) -> void:
     print(menu.name + " hidden")
 
-    if child_menus.all(_menu_is_closed):
+    if not _dialogue_in_progress and child_menus.all(_menu_is_closed):
         parent_menu.uncover()
 
 func _handle_child_menu_shown(menu: Menu) -> void:
